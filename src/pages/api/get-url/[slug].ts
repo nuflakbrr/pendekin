e7508ccb43908fd0d594a8d2053a7bbfc01a7f93
Naufal Akbar <kbrnugroho@gmail.com>
2022-06-15 15:49:38 +0700

    complete build

--- src/pages/api/get-url/[slug].ts
+++ src/pages/api/get-url/[slug].ts
@@ -3,38 +3,38 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "../../../db/client";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-    const slug = req.query["slug"];
+  const slug = req.query["slug"];
 
-    if (!slug || typeof slug !== "string") {
-        res.statusCode = 404;
+  if (!slug || typeof slug !== "string") {
+    res.statusCode = 404;
 
-        res.send(JSON.stringify({ message: "please use with a slug" }));
+    res.send(JSON.stringify({ message: "pls use with a slug" }));
 
-        return;
-    }
+    return;
+  }
 
-    const data = await prisma.shortLink.findFirst({
-        where: {
-            slug: {
-                equals: slug,
-            },
-        },
-    });
+  const data = await prisma.shortLink.findFirst({
+    where: {
+      slug: {
+        equals: slug,
+      },
+    },
+  });
 
-    if (!data) {
-        res.statusCode = 404;
+  if (!data) {
+    res.statusCode = 404;
 
-        res.send(JSON.stringify({ message: "slug not found" }));
+    res.send(JSON.stringify({ message: "slug not found" }));
 
-        return;
-    }
+    return;
+  }
 
-    res.setHeader("Content-Type", "application/json");
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-        "Cache-Control",
-        "s-maxage=1000000000, stale-while-revalidate"
-    );
+  res.setHeader("Content-Type", "application/json");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Cache-Control",
+    "s-maxage=1000000000, stale-while-revalidate"
+  );
 
-    return res.json(data);
+  return res.json(data);
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/pages/**/*.{ts,tsx}", "./src/components/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          950: "#18181b",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
